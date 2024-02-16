5426a89f64b92c3579a1bf44a5005b3cd46e15c0
Audddxd <chondarong555@gmail.com>
2024-02-16 21:50:20 +0700

    fix footer

--- client/src/layout/Footer.tsx
+++ client/src/layout/Footer.tsx
@@ -1,39 +1,63 @@
 import React from "react";
+import "../assets/style/foodter.css";
 
 const footer = () => {
   return (
     <div>
       <footer className="mx-auto pt-[5rem] pb-[2rem] justify-center bg-white">
-        <div className="mx-[160px] !justify-center items-center ">
-          <div className=" flex flex-wrap">
-            <div className="w-[33.333333%]">
+        <div className="-set-1 px-[160px] !justify-center items-center ">
+          <div className=" flex flex-wrap max-[1005px]:block">
+            <div className="w-[33.333333%] max-[1005px]:w-[100%] max-[1005px]:pb-10">
               <img className=" rounded-full w-[120px] mb-10 " src="/bio.jpg" />
-              <a className="block pb-3 font-bold text-[#818181]">Enrich Your Life with Our Passion</a>
+              <a className="block pb-3 font-bold text-[#818181]">
+                Enrich Your Life with Our Passion
+              </a>
               <a className="block pb-3 font-[300] text-[#818181]" href="">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit blanditiis reprehenderit cum cumque veritatis placeat repellendus aperiam iusto laborum sapiente ratione, voluptates distinctio voluptate ut adipisci earum voluptatum sint odit. 
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Impedit blanditiis reprehenderit cum cumque veritatis placeat
+                repellendus aperiam iusto laborum sapiente ratione, voluptates
+                distinctio voluptate ut adipisci earum voluptatum sint odit.
               </a>
             </div>
-            <div className="flex justify-between pl-[3rem] w-[66.666667%] text-left">
-              <div className="pl-[2rem]">
+            <div className="-set-2 max-[1005px]:w-[100%] max-[1005px]:!pl-0 flex justify-between pl-[3rem] w-[66.666667%] text-left">
+              <div className="pl-[2rem] max-[1005px]:!pl-0">
                 <a className=" block font-bold text-[17px] pb-[1.5rem]" href="">
                   Example 1
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 2
                 </a>
-                <a className=" block py-[8.5px] text-[17px]  text-[#818181]" href="">
-                Packages
+                <a
+                  className=" block py-[8.5px] text-[17px]  text-[#818181]"
+                  href=""
+                >
+                  Packages
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 4
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 5
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 6
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 7
                 </a>
               </div>
@@ -41,13 +65,22 @@ const footer = () => {
                 <a className=" block font-bold pb-[1.5rem]" href="">
                   Example 1
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 2
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 3
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 4
                 </a>
               </div>
@@ -55,28 +88,52 @@ const footer = () => {
                 <a className=" block font-bold pb-[1.5rem]" href="">
                   Example 1
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 2
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 3
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 4
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 5
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 6
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 7
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 8
                 </a>
-                <a className=" block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 9
                 </a>
               </div>
@@ -84,41 +141,82 @@ const footer = () => {
                 <a className=" block font-bold pb-[1.5rem]" href="">
                   Example 1
                 </a>
-                <a className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 2
                 </a>
-                <a className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 3
                 </a>
-                <a className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 4
                 </a>
-                <a className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 5
                 </a>
-                <a className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]" href="">
+                <a
+                  className=" font-[400] block py-[8.5px] text-[17px] text-[#818181]"
+                  href=""
+                >
                   Example 6
                 </a>
               </div>
             </div>
           </div>
-          <div className="font-[400] text-[17px] text-[#818181] pt-[10rem] text-center flex-wrap flex justify-between w-auto">     
+          <div className="fdropdown">
+              <div className="dropdown">
+                <button className="dropbtn">Menu1</button>
+                <div className="dropdown-content">
+                  <a >Link 1</a>
+                  <a >Link 2</a>
+                  <a >Link 3</a>
+                </div>
+              </div>
+              <div className="dropdown">
+                <button className="dropbtn">Menu2</button>
+                <div className="dropdown-content">
+                  <a >Link 1</a>
+                  <a >Link 2</a>
+                  <a >Link 3</a>
+                </div>
+              </div>
+              <div className="dropdown">
+                <button className="dropbtn">Menu3</button>
+                <div className="dropdown-content">
+                  <a >Link 1</a>
+                  <a >Link 2</a>
+                  <a >Link 3</a>
+                </div>
+              </div>
+            </div>
+          <div className="font-[400] text-[17px] text-[#818181] pt-[10rem] text-center flex-wrap flex justify-between w-auto">
             <span className="!text-left">
               {" "}
               © 2024 Green box. All Right Reserved.
             </span>
             <div className=" justify-center text-center flex-wrap flex">
               <a className="pl-[2.5rem] pt-0 pr-0" href="">
-              Cookie Policy
+                Cookie Policy
               </a>
               <a className="pl-[2.5rem] pt-0 pr-0" href="">
-              CCTV Policy
+                CCTV Policy
               </a>
               <a className="pl-[2.5rem] pt-0 pr-0" href="">
-              Privacy Policy
+                Privacy Policy
               </a>
-              <a className="pl-[2.5rem] pt-0 pr-0" href=""> 
-              Terms & Condition
+              <a className="pl-[2.5rem] pt-0 pr-0" href="">
+                Terms & Condition
               </a>
             </div>
           </div>
